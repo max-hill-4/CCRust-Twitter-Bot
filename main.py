9e4b7de2130458e83d5de6aca7e80e2f9c8f02a8
Max Hill <43377490+MHEmpire4@users.noreply.github.com>
2022-07-23 02:09:29 +0100

    final

--- main.py
+++ main.py
@@ -3,41 +3,43 @@ import pyyoutube
 import ffmpeg
 import random
 import datetime
-
-# Youtube API
-api = pyyoutube.Api(api_key='AIzaSyDv6m8oF5fKe7BrRTkbulND8R-gJEX5LgY')
-video = api.get_video_by_id(video_id='nZETPn1NoCM').items[0]
-upload_date = video.snippet.publishedAt
-
-# Date Handling
-current_date = datetime.datetime.combine(datetime.date.today(), datetime.datetime.min.time())
-upload_date = datetime.datetime.strptime(upload_date, '%Y-%m-%dT%H:%M:%SZ')
-time_since = str((current_date - upload_date).days)
-
-# Media Creation
-stream = ffmpeg.input('video.mp4', ss=random.randint(0,1200), t=10)
-stream = ffmpeg.drawtext(stream, text=f'CC hasnt uploaded in {time_since} days.', x=35, y=40,
-                        fontfile='font.ttf', fontsize=30, fontcolor='white')
-stream = ffmpeg.output(stream, 'output.gif')
-ffmpeg.run(stream, overwrite_output=True)
+import keys
 
 
-'''
-consumer_key = "rkdJ8eQ9aAwlLlyKFof6PsrWv"
+def get_date():
+    # Get the current date of youtube video and returns amount of days since upload.
+    api = pyyoutube.Api(api_key=keys.youtube_api)
+    video = api.get_video_by_id(video_id='nZETPn1NoCM').items[0]
+    upload_date = video.snippet.publishedAt
+    current_date = datetime.datetime.combine(datetime.date.today(), datetime.datetime.min.time())
+    upload_date = datetime.datetime.strptime(upload_date, '%Y-%m-%dT%H:%M:%SZ')
+    return str((current_date - upload_date).days)
 
-consumer_secret = "u7lcKeFiKjWBBW7JAeHDE1eNdogZGnCSykAL8dhse2EUmCkYiO"
 
-access_token = "775898153245704192-Ggativa8GI8KL7XfXakxPnobvsWl0R3"
+def post_tweet():
+    # Creates a tweet with the video and uploads it to twitter.
+    auth = tweepy.OAuthHandler(keys.consumer_key, keys.consumer_secret)
+    auth.set_access_token(keys.access_token, keys.access_token_secret)
+    api = tweepy.API(auth)
+    upload_result = api.media_upload('output.gif')
+    api.update_status(status="", media_ids=[upload_result.media_id_string])
+        
 
-access_token_secret = "Wmsa2G2WWUiWzeKzKyD7MZ4pA7tRsDC23xSYktRNC5w4k"
+def create_video():
+    # Creates a video and edits date ontop of it.
+    stream = ffmpeg.input('video.mp4', ss=random.randint(0,1200), t=7)
+    stream = ffmpeg.drawtext(stream, text=f'CC hasnt uploaded in {get_date()} days.', x=35, y=40,
+                           fontfile='font.ttf', fontsize=30, fontcolor='white')
 
-auth = tweepy.OAuth1UserHandler(
-consumer_key, consumer_secret, access_token, access_token_secret
-)
+    stream = ffmpeg.output(stream, 'output.gif')
+    ffmpeg.run(stream, overwrite_output=True)
 
-api = tweepy.API(auth)
+print('waiting...')
+while True:
+    if datetime.datetime.now().strftime('%H:%M:%S') == '00:00:00':
+        create_video()
+        post_tweet()
+        print('Tweeted!')
 
-public_tweets = api.home_timeline()
-for tweet in public_tweets:
-    print(tweet.text)
-    '''
+
+
